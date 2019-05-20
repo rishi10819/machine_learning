5549aefa02720587e3f9c246f9d27012ca72eb17
Riteshkumar Patel <rishi10819@gmail.com>
2019-05-20 19:17:51 -0400

    code update

--- Machine_Learning_Working_with_Matrices_2.R
+++ Machine_Learning_Working_with_Matrices_2.R
@@ -2,18 +2,6 @@ library(tidyverse)
 library(dslabs)
 mnist <- read_mnist()
 
-# List of 2
-# $ images: int [1:60000, 1:784] 0 0 0 0 0 0 0 0 0 0 ...
-# $ labels: int [1:60000] 5 0 4 1 9 2 1 3 1 4 ...
-#str(mnist$train)
-
-#x <- mnist$train$images[1:1000]
-#y <- mnist$train$label[1:1000]
-
-#y <- rowMeans(mnist$train$images>50 & mnist$train$images<205)
-#qplot(as.factor(mnist$train$labels), y, geom = "boxplot")
-
-
 boxplot(rowMeans(mnist$train$images)~mnist$train$labels) # generating boxplot
 #class(mnist$train$images) # "matrix"
 #class(mnist$train$labels) # "integer"
@@ -23,10 +11,8 @@ All_Pixles<-as.vector(mnist$train$images) # convert matrix to vector
 #class(All_Pixles)     # "integer"
 #typeof(All_Pixles)    # "integer"
 
+mean (All_Pixles >50 & All_Pixles<205) # 0.06183703
+
 hist(All_Pixles) # all area
 
 hist(All_Pixles[All_Pixles >50 & All_Pixles<205]) # grey area only
-
-z1 <- All_Pixles[All_Pixles >50 & All_Pixles<205]
-mean(z1) # 128.2618
-
